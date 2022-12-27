3905cebdf26a33184708b7960c4ddcea684b0e04
Vili Milner <vilimilner@gmail.com>
2022-12-27 18:03:36 -0500

    removed comments

--- src/utils/io/writeXML.ts
+++ src/utils/io/writeXML.ts
@@ -1,10 +1,8 @@
 import fs from "fs";
 import { Builder } from "xml2js";
 
-// TODO this should have better typing
 const writeXML = (path: string, data: any) => {
   // TODO add auto generated comment
-  // TODO remove xml declaration from top of file
   const builder = new Builder({ headless: true });
   const builtXML = builder.buildObject(data);
   return fs.promises.writeFile(path, builtXML);
