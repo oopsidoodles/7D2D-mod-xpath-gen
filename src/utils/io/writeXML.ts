52db12a286762cd70269382b3069ff13e9e0066b
Vili Milner <vilimilner@gmail.com>
2022-12-27 16:30:21 -0500

    removed header from generated xml

--- src/utils/io/writeXML.ts
+++ src/utils/io/writeXML.ts
@@ -5,7 +5,7 @@ import { Builder } from "xml2js";
 const writeXML = (path: string, data: any) => {
   // TODO add auto generated comment
   // TODO remove xml declaration from top of file
-  const builder = new Builder();
+  const builder = new Builder({ headless: true });
   const builtXML = builder.buildObject(data);
   return fs.promises.writeFile(path, builtXML);
 };
